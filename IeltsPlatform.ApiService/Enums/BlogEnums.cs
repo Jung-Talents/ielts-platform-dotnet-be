f0eca7d8d80c0480be9c045ef2bc94c34874851a
phucchau050303 <phcchu88@gmail.com>
2025-11-03 23:04:04 +1100

    feat(enums) : add BlogStatus and BlogTheme, used by Blog entity

---
+++ IeltsPlatform.ApiService/Enums/BlogEnums.cs
@@ -0,0 +1,17 @@
+namespace IeltsPlatform.ApiService.Enums
+{
+    public enum BlogStatus
+    {
+        Draft = 0,
+        Published = 1,
+        Archived = 2
+    }
+
+    public enum BlogTheme
+    {
+        Listening = 0,
+        Reading = 1,
+        Speaking = 2,
+        Writing = 3
+    }
+}
