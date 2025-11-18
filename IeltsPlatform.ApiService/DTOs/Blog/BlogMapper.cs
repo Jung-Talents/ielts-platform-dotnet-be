581892765ed428ca9356bf2c5aaa5da8ccdc2e4a
Ducnapan <ducnapan2015@gmail.com>
2025-11-18 16:05:12 +1100

    add DeleteBlog to BlogsController.cs

---
+++ IeltsPlatform.ApiService/DTOs/Blog/BlogMapper.cs
@@ -0,0 +1,16 @@
+﻿namespace IeltsPlatform.ApiService.DTOs.Blog
+{
+    public static class BlogMapper
+    {
+        public static Entities.Blog CreateCategoryFromDto(CreateBlogRequest dto)
+        {
+            return Entities.Blog.Create(dto.Name,
+                dto.Content,
+                dto.Status,
+                dto.Theme,
+                createdAt: DateTimeOffset.UtcNow,
+                updatedAt: DateTimeOffset.UtcNow
+            );
+        }
+    }
+}
---
+++ IeltsPlatform.ApiService/Entities/Blog.cs
@@ -0,0 +1,31 @@
+﻿using IeltsPlatform.ApiService.Enums;
+
+namespace IeltsPlatform.ApiService.Entities
+{
+    public sealed class Blog
+    {
+        private Blog() { }
+        private Blog(string name, string content, BlogStatus status, BlogTheme theme, DateTimeOffset createdAt, DateTimeOffset? updatedAt)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Content = content;
+            Status = status;
+            Theme = theme;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+        }
+        public static Blog Create(string name, string content, BlogStatus status, BlogTheme theme, DateTimeOffset createdAt, DateTimeOffset? updatedAt)
+        {
+            return new Blog(name, content, status, theme, createdAt, updatedAt);
+        }
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public BlogStatus Status { get; set; }
+        public BlogTheme Theme { get; set; }
+        public DateTimeOffset? UpdatedAt { get; set; } = DateTimeOffset.UtcNow;
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+        public DateTimeOffset? DeletedAt { get; set; }
+    }
+}
